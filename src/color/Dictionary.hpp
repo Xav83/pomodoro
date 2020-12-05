cbeb0e1684fee6f54a77919e9d009c2385e4d375
Xav83 <x.jouvenot@gmail.com>
2020-12-05 13:16:50 +0100

    Adds some classic colors to please more users

--- src/color/Dictionary.hpp
+++ src/color/Dictionary.hpp
@@ -4,8 +4,11 @@
 #include <array>
 
 namespace pomodoro::color {
-constexpr std::array<pomodoro::color::Set, 2> dictionary{{
+constexpr std::array<pomodoro::color::Set, 5> dictionary{{
     {fmt::color::cadet_blue, fmt::color::golden_rod},
     {fmt::color::light_coral, fmt::color::light_blue},
+    {fmt::color::white, fmt::color::white},
+    {fmt::color::green, fmt::color::green},
+    {fmt::color::black, fmt::color::black},
 }};
 }
