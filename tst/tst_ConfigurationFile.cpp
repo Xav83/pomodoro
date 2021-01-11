d4cf596dde16b50fa3251d89458a7b00cf1ca923
Xav83 <x.jouvenot@gmail.com>
2021-01-11 21:48:14 +0100

    Adds a test about trying to pass a folder to the ConfigurationFile class.

--- tst/tst_ConfigurationFile.cpp
+++ tst/tst_ConfigurationFile.cpp
@@ -7,3 +7,10 @@ TEST(tst_ConfigurationFile, EmptyPathAsConfigurationFile) {
   // NOLINTNEXTLINE(cppcoreguidelines-pro-type-vararg)
   EXPECT_DEBUG_DEATH(pomodoro::ConfigurationFile conf_file(path), "");
 }
+
+// NOLINTNEXTLINE
+TEST(tst_ConfigurationFile, FolderAsConfigurationFile) {
+  std::filesystem::path path = "/folder/";
+  // NOLINTNEXTLINE(cppcoreguidelines-pro-type-vararg)
+  EXPECT_DEBUG_DEATH(pomodoro::ConfigurationFile conf_file(path), "");
+}
