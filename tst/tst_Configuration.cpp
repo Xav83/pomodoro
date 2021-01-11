fa139a92c9110995c41aa9cd468a64a7d263397a
Xav83 <x.jouvenot@gmail.com>
2021-01-11 15:04:33 +0100

    Tests the custom configuration using the constructor

--- tst/tst_Configuration.cpp
+++ tst/tst_Configuration.cpp
@@ -2,8 +2,7 @@
 #include "color/Dictionary.hpp"
 #include "gtest/gtest.h"
 
-// NOLINTNEXTLINE(cppcoreguidelines-special-member-functions, cert-err58-cpp,
-// cppcoreguidelines-owning-memory)
+// NOLINTNEXTLINE
 TEST(tst_Configuration, DefaultConfiguration) {
   pomodoro::Configuration conf;
 
@@ -16,4 +15,18 @@ TEST(tst_Configuration, DefaultConfiguration) {
                                   pomodoro::color::Id::default_1)]);
   EXPECT_EQ(conf.getLongBreakTime(),
             std::chrono::minutes(pomodoro::numbers::default_long_break_time));
+}
+
+// NOLINTNEXTLINE
+TEST(tst_Configuration, CustomConfigurationConstuctor) {
+  pomodoro::Configuration conf(
+      pomodoro::color::Id::default_2, std::chrono::minutes(42),
+      std::chrono::minutes(666), std::chrono::minutes(13));
+
+  EXPECT_EQ(conf.getColorId(), pomodoro::color::Id::default_2);
+  EXPECT_EQ(conf.getWorkTime(), std::chrono::minutes(42));
+  EXPECT_EQ(conf.getBreakTime(), std::chrono::minutes(666));
+  EXPECT_EQ(conf.getColors(), pomodoro::color::dictionary[static_cast<size_t>(
+                                  pomodoro::color::Id::default_2)]);
+  EXPECT_EQ(conf.getLongBreakTime(), std::chrono::minutes(13));
 }
