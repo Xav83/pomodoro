1f317d9274aa94c633603260ad5e6a25054c24a3
Xav83 <x.jouvenot@gmail.com>
2021-01-11 13:52:12 +0100

    Formats the test files too

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
 
 format:
-	@(clang-format -i src/*pp src/color/*pp src/utility/*pp)
+	@(clang-format -i src/*pp src/color/*pp src/utility/*pp tst/*pp)
 	@(cmake-format -i CMakeLists.txt .cmake/*cmake)
--- tst/tst_Configuration.cpp
+++ tst/tst_Configuration.cpp
@@ -1,14 +1,19 @@
-#include "gtest/gtest.h"
 #include "Configuration.hpp"
 #include "color/Dictionary.hpp"
+#include "gtest/gtest.h"
 
-// NOLINTNEXTLINE(cppcoreguidelines-special-member-functions, cert-err58-cpp, cppcoreguidelines-owning-memory)
+// NOLINTNEXTLINE(cppcoreguidelines-special-member-functions, cert-err58-cpp,
+// cppcoreguidelines-owning-memory)
 TEST(tst_Configuration, DefaultConfiguration) {
-pomodoro::Configuration conf;
+  pomodoro::Configuration conf;
 
   EXPECT_EQ(conf.getColorId(), pomodoro::color::Id::default_1);
-  EXPECT_EQ(conf.getWorkTime(), std::chrono::minutes(pomodoro::numbers::default_work_time));
-  EXPECT_EQ(conf.getBreakTime(), std::chrono::minutes(pomodoro::numbers::default_break_time));
-  EXPECT_EQ(conf.getColors(), pomodoro::color::dictionary[static_cast<size_t>(pomodoro::color::Id::default_1)]);
-  EXPECT_EQ(conf.getLongBreakTime(), std::chrono::minutes(pomodoro::numbers::default_long_break_time));
+  EXPECT_EQ(conf.getWorkTime(),
+            std::chrono::minutes(pomodoro::numbers::default_work_time));
+  EXPECT_EQ(conf.getBreakTime(),
+            std::chrono::minutes(pomodoro::numbers::default_break_time));
+  EXPECT_EQ(conf.getColors(), pomodoro::color::dictionary[static_cast<size_t>(
+                                  pomodoro::color::Id::default_1)]);
+  EXPECT_EQ(conf.getLongBreakTime(),
+            std::chrono::minutes(pomodoro::numbers::default_long_break_time));
 }
