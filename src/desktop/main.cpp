9150f8146d9717441eb536ff7c1d993f3fe975dd
Xav83 <x.jouvenot@gmail.com>
2021-01-29 00:28:58 +0100

    Displays the pomodoro timer in the window

--- src/desktop/main.cpp
+++ src/desktop/main.cpp
@@ -1,8 +1,10 @@
 #include "Fonts.hpp"
+#include "shared/Configuration.hpp"
 #include "shared/Pomodoro.hpp"
 #include <SFML/Graphics.hpp>
 #include <SFML/Window.hpp>
 #include <cassert>
+#include <fmt/chrono.h>
 
 int main() {
   sf::RenderWindow window(sf::VideoMode(800, 600), "Pomodoro");
@@ -14,6 +16,11 @@ int main() {
   text.setFont(font);
   text.setCharacterSize(24);
 
+  pomodoro::Configuration configuration;
+  Pomodoro pomodoro(
+      configuration, []() {}, []() {}, []() {});
+  pomodoro.run();
+
   while (window.isOpen()) {
     sf::Event event;
     while (window.pollEvent(event)) {
@@ -21,6 +28,11 @@ int main() {
         window.close();
     }
     window.clear();
+
+    const auto &timer = pomodoro.getCurrentTimer();
+    text.setString(fmt::format("Timer {} : {:%M:%S}", timer.getName(),
+                               timer.getRemainingTime()));
+
     window.draw(text);
     window.display();
   }
